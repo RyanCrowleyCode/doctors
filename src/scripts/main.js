0d1df0f7dd7f557d89ec6dd418cdd7971797a80c
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-31 09:19:21 -0500

    lightning exercise one complete. Factory Function for newDoctor created and tested.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,21 @@
 // Doctor's name
 // Specialty (Oncologist, pediatrician, etc...)
 // Address of practice
+
+const newDoctor = (name, specialty, address) => ({
+    "name": name,
+    "specialty": specialty,
+    "address": address
+})
+
+const dave = newDoctor("Dave", "Oncology", "123 Sesame Street")
+
+console.log(dave)
+console.log(dave.name)
+console.log(dave.specialty)
+console.log(dave.address)
+
+
 // Lightning Exercise 2: Write a factory function that creates an object that represents a pet. The function should accept two arguments.
 
 // Pet name
